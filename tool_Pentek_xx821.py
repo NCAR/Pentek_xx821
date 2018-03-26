68025edc69ad6619ab706a9fa1d94e57018619d1
Chris Burghart <burghart@ucar.edu>
2018-03-26 15:40:02 -0600

    First cut at a SCons tool file for Pentek_xx821

---
+++ tool_Pentek_xx821.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+import eol_scons
+
+# Any environment setting will be the default, but it can be overridden by
+# setting the configuration variable.
+
+variables = eol_scons.GlobalVariables()
+
+requiredTools = [
+    'logx',
+    'Navigator_xx821', # Pentek's Navigator BSP
+    'doxygen',
+]
+
+env = Environment(requiredTools = ['default'] + requiredTools)
+
+variables.Update(env)
+
+libsources = Split("""
+Pentek_xx821.cpp
+""")
+
+headers = Split("""
+Pentek_xx821.h
+""")
+
+libpentek = env.Library('Pentek_xx821', libsources)
+Default(libpentek)
+
+env['DOXYFILE_DICT'].update({'PROJECT_NAME':'Pentek_xx821'})
+html = env.Apidocs(libsources + headers)
+Default(html)
+
+# Build the test programs
+SConscript("test/SConscript")
+
+thisdir = env.Dir('.').srcnode().abspath
+def Pentek_xx821(env):
+    env.AppendUnique(CPPPATH = [thisdir])
+    env.AppendLibrary('Pentek_xx821')
+    env.AppendDoxref('Pentek_xx821')
+    env.Require(requiredTools)
+
+Export('Pentek_xx821')
