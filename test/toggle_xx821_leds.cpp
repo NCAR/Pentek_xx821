70a74c4453ea2e1b58de386abc4af77800014621
Chris Burghart <burghart@ucar.edu>
2018-03-27 16:55:00 -0600

    Remove old commented code and clean up

--- test/toggle_xx821_leds.cpp
+++ test/toggle_xx821_leds.cpp
@@ -111,18 +111,21 @@ main(int argc, char** argv)
 
     // Open a board, providing for user selection if there's more than one
     // board.
-    void * boardHandle = NAV_BoardSelect(numBoards, 0, boardList[0], 0);
-    if (boardHandle) {
+    void * boardPtr = NAV_BoardSelect(numBoards, 0, boardList[0], 0);
+    NAV_BOARD_RESRC * boardResource = reinterpret_cast<NAV_BOARD_RESRC *>(boardPtr);
+
+    if (boardResource) {
         DLOG << "Opened xx821 board";
     } else {
         ELOG << "Failed to open board!";
         exit(1);
     }
 
-    // Alternately toggle the USR LED and MAS LED _nToggles times.
-    NAV_BOARD_RESRC * boardResource = reinterpret_cast<NAV_BOARD_RESRC *>(boardHandle);
+    // Alternately light the green user (USR) LED and amber master (MAS) LED
+    // _nToggles times.
     volatile uint32_t * BAR0Base =
             reinterpret_cast<volatile uint32_t *>(boardResource->pciInfo.BAR0Base);
+
     for (int t = 0; t < _nToggles; t++) {
         ILOG << "Cycle " << t + 1 << " of " << _nToggles;
         // USR LED on/MAS LED off for half of the toggle period
@@ -157,138 +160,6 @@ main(int argc, char** argv)
                     NAV_BUS_MSTR_STAND_ALONE);
 
     // Release the board and close Navigator cleanly
-    NAV_BoardClose(boardHandle);
+    NAV_BoardClose(boardResource);
     NAV_BoardFinish();
 }
-
-//  // Initialize the library
-//
-//  DWORD dwStatus = PTK714X_LibInit();
-//  if (PTK714X_STATUS_OK != dwStatus) {
-//    return (exitHandler (1, NULL));
-//  }
-//  
-//  // Find and open the next PTK714X device
-//  // user will be asked to pick the device num
-//  
-//  DWORD BAR0Base;
-//  DWORD BAR2Base;
-//  DWORD slot = -1; // forces user interaction if more than 1 pentek
-//  void *hDev = PTK714X_DeviceFindAndOpen(&slot, &BAR0Base, &BAR2Base);
-//  if (hDev == NULL) {
-//    return (exitHandler (2, hDev));
-//  }
-//  
-//  // Initialize the 7142 register address table
-//  
-//  P7142_REG_ADDR p7142Regs;  /* 7142 register table */
-//  P7142InitRegAddr(BAR0Base, BAR2Base, &p7142Regs);
-//
-//  // check if module is a 7142
-//
-//  unsigned int moduleId;
-//  P7142_GET_MODULE_ID(p7142Regs.BAR2RegAddr.idReadout, moduleId);
-//  if (moduleId != P7142_MODULE_ID) {
-//    cerr << "Pentek card " << slot + 1
-//         << " is not a 7142!" << endl;
-//    cerr << "Expected 0x" << hex << P7142_MODULE_ID << 
-//      ", and got 0x" << moduleId << dec << endl;
-//    return -1;
-//  }
-//
-//  // print status
-//
-//  cerr << "Found Pentek 7142 device";
-//  cerr << "  device addr: " << hex << hDev << endl;
-//  cerr << "  slot: " << dec << slot << endl;
-//  cerr << "  BAR0Base: " << hex << (void *)BAR0Base;
-//  cerr << "  BAR2Base: " << hex << (void *)BAR2Base;
-//  cerr << dec;
-//  cerr << endl;
-//
-//  // get master bus controls
-//
-//  uint32_t masterBusAControl;
-//  P7142_REG_READ(BAR2Base + P7142_MASTER_BUS_A_CONTROL, masterBusAControl);
-//  
-//  uint32_t masterBusBControl;
-//  P7142_REG_READ(BAR2Base + P7142_MASTER_BUS_B_CONTROL, masterBusBControl);
-//
-//  cerr << "Initial state:" << endl;
-//  cerr << "  masterBusAControl: " << hex << masterBusAControl << endl;
-//  cerr << "  masterBusBControl: " << hex << masterBusBControl << endl;
-//  cerr << dec;
-//
-//  // toggle master control on and off, to toggle LEDs
-//
-//  uint32_t bothOnA = masterBusAControl | 0x00000003;
-//  uint32_t bothOnB = masterBusBControl | 0x00000003;
-//  
-//  uint32_t masterOnA = bothOnA | 0x00000001;
-//  uint32_t masterOnB = bothOnB | 0x00000001;
-//
-//  uint32_t masterOffA = bothOnA & 0xfffffffe;
-//  uint32_t masterOffB = bothOnB & 0xfffffffe;
-//
-//  uint32_t termOnA = bothOnA | 0x00000002;
-//  uint32_t termOnB = bothOnB | 0x00000002;
-//  
-//  uint32_t termOffA = bothOnA & 0xfffffffd;
-//  uint32_t termOffB = bothOnB & 0xfffffffd;
-//
-//  useconds_t sleepTime = (int) (_waitSecs * 1.0e6);
-//
-//  cerr << "--->> Setting all on" << endl;
-//    
-//  P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_A_CONTROL, bothOnA);
-//  P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_B_CONTROL, bothOnB);
-//
-//  for (int ii = 0; ii < _nToggles; ii++) {
-//
-//    cerr << "--->> Toggling master off" << endl;
-//    
-//    P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_A_CONTROL, masterOffA);
-//    P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_B_CONTROL, masterOffB);
-//
-//    usleep(sleepTime);
-//
-//    cerr << "--->> Toggling master on" << endl;
-//    
-//    P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_A_CONTROL, masterOnA);
-//    P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_B_CONTROL, masterOnB);
-//    
-//    usleep(sleepTime);
-//    
-//    cerr << "--->> Toggling term off" << endl;
-//    
-//    P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_A_CONTROL, termOffA);
-//    P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_B_CONTROL, termOffB);
-//
-//    usleep(sleepTime);
-//
-//    cerr << "--->> Toggling term on" << endl;
-//    
-//    P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_A_CONTROL, termOnA);
-//    P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_B_CONTROL, termOnB);
-//    
-//    usleep(sleepTime);
-//    
-//  }
-//
-//  // reset to the initial state
-//    
-//  // cerr << "--->> Resetting to initial state" << endl;
-//  // P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_A_CONTROL, masterBusAControl);
-//  // P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_B_CONTROL, masterBusBControl);
-//
-//  // set all on to exit
-//    
-//  cerr << "--->> Setting all on" << endl;
-//    
-//  P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_A_CONTROL, bothOnA);
-//  P7142_REG_WRITE(BAR2Base + P7142_MASTER_BUS_B_CONTROL, bothOnB);
-//
-//  // return
-//
-//  return (exitHandler (0, hDev));
-
