40d27e2e8c5c0e07aa91de11834bdd75f108e0a1
Chris Burghart <burghart@ucar.edu>
2018-03-27 16:27:44 -0600

    Add empty dict if DOXYFILE_DICT is not in the env

--- tool_Pentek_xx821.py
+++ tool_Pentek_xx821.py
@@ -14,7 +14,7 @@ requiredTools = [
     'doxygen',
 ]
 
-env = Environment(requiredTools = ['default'] + requiredTools)
+env = Environment(tools = ['default'] + requiredTools)
 
 variables.Update(env)
 
@@ -29,12 +29,11 @@ Pentek_xx821.h
 libpentek = env.Library('Pentek_xx821', libsources)
 Default(libpentek)
 
+if not 'DOXYFILE_DICT' in env:
+    env['DOXYFILE_DICT'] = {}
 env['DOXYFILE_DICT'].update({'PROJECT_NAME':'Pentek_xx821'})
 html = env.Apidocs(libsources + headers)
 Default(html)
-
-# Build the test programs
-SConscript("test/SConscript")
 
 thisdir = env.Dir('.').srcnode().abspath
 def Pentek_xx821(env):
@@ -44,3 +43,6 @@ def Pentek_xx821(env):
     env.Require(requiredTools)
 
 Export('Pentek_xx821')
+
+# Build the test programs
+SConscript("test/SConscript")
